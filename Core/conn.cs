1f7ea3e9443207a0c96386edcffd269bb44d1141
sso2014 <santiago.ozuna@hotmail.com>
2014-09-01 10:55:15 -0300

    Core 1.0

---
+++ Core/conn.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Data.OleDb;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Core
+{
+    public class conn
+    {
+
+        private string StringConnection = "";
+
+        public OleDbConnection GetOldbConnection()
+        {
+            try
+            {
+
+                OleDbConnection con = new OleDbConnection();
+                
+                con.Open();
+
+                if (con.State == ConnectionState.Open)
+                {
+                    return con;
+                }
+                else {
+                    return null;
+                }
+            }
+            catch (Exception exp) {
+                return null;
+            }
+        }
+    }
+}
